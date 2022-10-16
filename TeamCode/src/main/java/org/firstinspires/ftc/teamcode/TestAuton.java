1820b23d90aa655621c8fca11e55833a99a79fad
gt74745 <garrison.m.taylor@gmail.com>
2022-10-16 12:55:16 -0500

    Fix move

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -10,24 +10,31 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @Autonomous
 public class TestAuton extends LinearOpMode
 {
     @Override
-    public void runOpMode() throws InterruptedException
+    public void runOpMode()
     {
         Config config = new Config.Builder()
                 .setDriveMotors("m0", "m1", "m2", "m3")
                 .setIMU("imu")
-                .addAccessory(new Accessory(AccessoryType.WEBCAM, "webcam"))
+//                .addAccessory(new Accessory(AccessoryType.WEBCAM, "webcam"))
                 .setPIDCoefficients(new PIDCoefficients(0.01, 0.00003, 0), new PIDCoefficients(0.25, 0.0004, 0))
+                .setOpMode(this)
                 .build();
 
         HardwareManager manager = new HardwareManager(config, hardwareMap);
 
         Pipeline pipeline = new Pipeline.Builder(manager)
-                .addLinearPath(new Position(0, 0, 0), new Position(100, 0, 0), new Position(100, 100, Math.PI))
+                .addLinearPath(new Position(0, 0, 0), new Position(1000, 0, 0), new Position(1000, 1000, 180))
                 .build();
+
+        telemetry.addLine("test");
+        telemetry.update();
+        waitForStart();
 
         pipeline.execute();
     }
