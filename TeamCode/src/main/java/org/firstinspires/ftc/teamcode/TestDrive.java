185e15a6ac9fa1907769ae49115c69fdb455d92b
danielhuinda <huindam@gmail.com>
2022-11-08 15:01:28 -0600

    Added imu reset action to A button.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import com.chsrobotics.ftccore.geometry.Position;
+import com.chsrobotics.ftccore.actions.integratedactions.SampleIMUAction;
 import com.chsrobotics.ftccore.hardware.HardwareManager;
 import com.chsrobotics.ftccore.hardware.config.Config;
-import com.chsrobotics.ftccore.pipeline.Pipeline;
 import com.chsrobotics.ftccore.teleop.Drive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 @TeleOp
 public class TestDrive extends LinearOpMode {
@@ -17,6 +15,7 @@ public class TestDrive extends LinearOpMode {
         Config config = new Config.Builder()
                 .setDriveMotors("m0", "m1", "m2", "m3")
                 .setIMU("imu")
+                .setTeleopValues(1, 1)
 //                .addAccessory(new Accessory(AccessoryType.WEBCAM, "webcam"))
                 .setOpMode(this)
                 .build();
@@ -24,6 +23,7 @@ public class TestDrive extends LinearOpMode {
         HardwareManager manager = new HardwareManager(config, hardwareMap);
 
         Drive drive = new Drive.Builder(manager)
+                .bindActionToButton(Drive.Builder.GamepadButtons.A, new SampleIMUAction(manager), 1)
                 .Build();
 
         waitForStart();
