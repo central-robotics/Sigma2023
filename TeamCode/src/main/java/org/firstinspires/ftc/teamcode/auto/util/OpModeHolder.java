6dc1fa16292baa96ecf122452bfcdbb6503c19c7
gt74745 <garrison.m.taylor@gmail.com>
2023-01-09 09:41:15 -0600

    Kickapoo meet 2 changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/util/OpModeHolder.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.auto.util;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-public class OpModeHolder {
-    public static LinearOpMode opMode;
-}
